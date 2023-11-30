93d5999dcb64e7f935089df3b02703a26cca40c8
August Wetterau <august.wetterau1742@gmail.com>
2023-11-30 13:50:19 -0800

    added choose type dropdown

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
-function Api({ muscle }) {
+function Api({ addOn }) {
   const [muscleData, setMuscleData] = useState([])
   useEffect(() => {
     const apiKey = 'Xrl51qDFfoM9efpFLe3fQA==99IryFKClz7aqFze'
 
-    fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
+    fetch(`https://api.api-ninjas.com/v1/exercises?${addOn}`, {
       method: 'GET',
       headers: {
         'X-Api-Key': apiKey,
@@ -30,9 +30,14 @@ function Api({ muscle }) {
   return (
     <div>
       {muscleData.map((item, index) => (
-        <p key={index} value={index}>
-          {item.name}
-        </p>
+        <div>
+          <p className="exerciseName" key={index} value={index}>
+            Name: {item.name}
+          </p>
+          <p classname="characteristic">Equipment Needed: {item.equipment}</p>
+          <p classname="characteristic">Difficulty: {item.difficulty}</p>
+          <p classname="characteristic">Instructions: {item.instructions}</p>
+        </div>
       ))}
     </div>
   )
--- src/components/Data.js
+++ src/components/Data.js
@@ -2,39 +2,76 @@ import Api from './Api.js'
 import { useState, useEffect } from 'react'
 
 function Data() {
-  const [muscle, setMuscle] = useState('abdominals')
+  const [apiParameter, setApiParameter] = useState(undefined)
+  const [listType, setListType] = useState('muscle')
 
   return (
     <div>
       <p>
+        List By:
         <select
-          name="muscle"
-          id="muscle"
-          value={undefined}
-          onChange={e => setMuscle(e.target.value)}
+          name="type"
+          id="type"
+          value={listType}
+          onChange={e => {
+            setListType(e.target.value)
+            setApiParameter(undefined)
+          }}
           className="select"
         >
-          <option value="" disabled={muscle !== undefined}>
-            Target Muscle
-          </option>
-          <option value="abdominals">Abdominals</option>
-          <option value="abductors">Abductors</option>
-          <option value="adductors">Adductors</option>
-          <option value="biceps">Biceps</option>
-          <option value="calves">Calves</option>
-          <option value="chest">Chest</option>
-          <option value="forearms">Forearms</option>
-          <option value="hamstrings">Hamstrings</option>
-          <option value="lats">Lats</option>
-          <option value="lower_back">Lower_back</option>
-          <option value="middle_back">Middle_back</option>
-          <option value="neck">Neck</option>
-          <option value="quadriceps">Quadriceps</option>
-          <option value="traps">Traps</option>
-          <option value="triceps">Triceps</option>
+          <option value="muscle">Muscle Group</option>
+          <option value="type">Exercise Type</option>
         </select>
+        {listType == 'muscle' && (
+          <select
+            name="muscle"
+            id="muscle"
+            value={apiParameter}
+            onChange={e => setApiParameter(e.target.value)}
+            className="select"
+          >
+            <option value="" disabled={apiParameter !== undefined}>
+              Target Muscle
+            </option>
+            <option value="muscle=abdominals">Abdominals</option>
+            <option value="muscle=abductors">Abductors</option>
+            <option value="muscle=adductors">Adductors</option>
+            <option value="muscle=biceps">Biceps</option>
+            <option value="muscle=calves">Calves</option>
+            <option value="muscle=chest">Chest</option>
+            <option value="muscle=forearms">Forearms</option>
+            <option value="muscle=hamstrings">Hamstrings</option>
+            <option value="muscle=lats">Lats</option>
+            <option value="muscle=lower_back">Lower_back</option>
+            <option value="muscle=middle_back">Middle_back</option>
+            <option value="muscle=neck">Neck</option>
+            <option value="muscle=quadriceps">Quadriceps</option>
+            <option value="muscle=traps">Traps</option>
+            <option value="muscle=triceps">Triceps</option>
+          </select>
+        )}
+        {listType == 'type' && (
+          <select
+            name="cardio"
+            id="cardio"
+            value={undefined}
+            onChange={e => setApiParameter(e.target.value)}
+            className="select"
+          >
+            <option value="" disabled={apiParameter !== undefined}>
+              Type
+            </option>
+            <option value="type=cardio">Cardio</option>
+            <option value="type=olympic_weightlifting">Olympic Weightlifting</option>
+            <option value="type=plyometrics">Plyometrics</option>
+            <option value="type=powerlifting">Powerlifting</option>
+            <option value="type=strength">Strength</option>
+            <option value="type=stretching">Stretching</option>
+            <option value="type=strongman">Strongman</option>
+          </select>
+        )}
       </p>
-      <Api muscle={muscle} /> {/*   Doesnt update when muscle is changed   */}
+      {apiParameter !== undefined && <Api addOn={apiParameter} />} {/*   Doesnt update when muscle/type is changed   */}
     </div>
   )
 }
