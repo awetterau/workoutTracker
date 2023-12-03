af763a5b7ba1fbedcd8816d991416dece4f067f3
Brian Bowers <bmbowers22@gmail.com>
2023-12-03 11:57:49 -0800

    database for workout and public checkbox

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { addWorkoutDb } from '../services/workoutService.js'
+import { ExerciseData, addWorkoutDb } from '../services/workoutService.js'
 import { useAuth } from '../services/authService.js'
 import { Link, Navigate, useNavigate } from 'react-router-dom'
 
@@ -7,12 +7,29 @@ function Data() {
   const user = useAuth()
   const navigate = useNavigate()
   const [exercises, setExercises] = useState([])
-  const [name, setName] = useState(null)
-  const [reps, setReps] = useState(null)
-  const [weight, setWeight] = useState(null)
+  const [name, setName] = useState('')
+  const [reps, setReps] = useState('')
+  const [weight, setWeight] = useState('')
+  const [workoutName, setWorkoutName] = useState('')
+
+  const [fieldError, setFieldError] = useState(false)
+  const [nameError, setNameError] = useState(false)
+  const [noDataError, setNoDataError] = useState(false)
+  const [isChecked, setIsChecked] = useState(false)
 
   const handleAddWorkout = () => {
-    addWorkoutDb(user, 'test', 'test')
+    if (workoutName) {
+      if (exercises.length > 0) {
+        setWorkoutName('')
+        //add an option whether or not to add to public database
+        addWorkoutDb(user, workoutName, exercises, isChecked)
+        console.log('calling add workout')
+        setNoDataError(false)
+      } else setNoDataError(true)
+      setNameError(false)
+    } else {
+      setNameError(true)
+    }
   }
 
   const handleSuggestionClick = () => {
@@ -21,18 +38,27 @@ function Data() {
 
   const handleAddExercise = () => {
     if (name && reps && weight) {
-      setExercises([...exercises, { name, reps, weight }])
-      console.log(exercises)
-      setReps(null)
-      setWeight(null)
-      setName(null)
-    }
+      setExercises(prevExercises => [...prevExercises, { name, reps, weight }])
+      setReps('')
+      setWeight('')
+      setName('')
+      setFieldError(false)
+    } else setFieldError(true)
   }
 
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked)
+  }
   return (
     <div>
       {user ? (
         <div className="Data">
+          <span>
+            Workout Name <input value={workoutName} onChange={e => setWorkoutName(e.target.value)}></input>
+            Make workout public<input type="checkbox" value={'isChecked'} onChange={handleCheckboxChange}></input>
+          </span>
+          {nameError && <p>Error: must enter a name</p>}
+          {noDataError && <p>Error: must enter at least one exercise</p>}
           <span>
             Exercise Name<input value={name} onChange={e => setName(e.target.value)}></input>
           </span>
@@ -42,8 +68,9 @@ function Data() {
           <span>
             Weight<input value={weight} onChange={e => setWeight(e.target.value)}></input>
           </span>
+          {fieldError && <p>Error: all fiels must be completed</p>}
           <button onClick={handleAddExercise}>Add Exercise</button>
-
+          <ExerciseData array={exercises} />
           <button onClick={handleAddWorkout}>Add workout to db</button>
           <button onClick={handleSuggestionClick}>Get a suggestion</button>
         </div>
--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,19 +1,36 @@
 import { auth, db } from '../firebaseConfig'
 import { addDoc, collection, doc, getDoc, setDoc } from 'firebase/firestore'
 
-export async function addWorkoutDb(user, workout, name) {
-  console.log(user)
+export async function addWorkoutDb(user, name, workout, bool) {
   const uid = user.uid
   const displayName = user.displayName
 
   const data = { uid, displayName, workout }
-  console.log(data)
-  await setDoc(doc(db, 'workouts', name), {
-    data
+
+  console.log('is checked ----', bool)
+  if (bool) {
+    await setDoc(doc(db, 'publicWorkouts', name), {
+      data
+    })
+  }
+  await setDoc(doc(db, 'users', uid, 'workouts', name), {
+    workout
   })
 }
 
-export function addExercise() {}
+export function ExerciseData(array) {
+  return array.array ? (
+    array.array.map((item, index) => (
+      <div key={index}>
+        <p>Name: {item.name}</p>
+        <p>Reps: {item.reps}</p>
+        <p>Weight: {item.weight}</p>
+      </div>
+    ))
+  ) : (
+    <p>No data</p>
+  )
+}
 
 // export async function getUserByUid(uid) {
 //   const docRef = doc(db, 'users', uid.uid)
